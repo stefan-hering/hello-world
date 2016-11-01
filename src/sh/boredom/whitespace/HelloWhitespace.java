50c32a33394df334fbe52b66fdc23f99dabdaf4e
stefan-hering <stefanvhering@gmail.com>
2016-11-01 11:49:24 +0100

    added HelloWhitespace.java

---
+++ src/sh/boredom/whitespace/HelloWhitespace.java
@@ -0,0 +1,29 @@
+package   sh.boredom.whitespace;	  	   
+	
+     		  	 	
+/**	
+ A completely normal class 		 		  
+	
+ It just prints Hello World		 		  
+*/	
+public class HelloWhitespace {  		 				
+	
+     	 		  
+	/**
+     This	method prints Hello World!  
+	*/
+     public	static	void	main(String[] args)	{		
+	
+     	String	hello =	"Hello"	;		
+	
+     	String	world	= "World"; 	 
+	
+     	System.out.println(	hello+" "	+	world + "!");
+	
+     		  	  
+	
+     }	    	
+	
+  
+}
+
